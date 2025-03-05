b714096fd180662bf906f02b13221f7cf6c17251
Fxploit157 <foreignexploit@gmail.com>
2025-03-05 12:09:53 +0200

    modified and created a chatbot with the existing files

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
-const button = document.querySelector("#button") ;
+const textSpace = document.querySelector("#text_space") ;
+const button = document.querySelector("#send_button") ;
+const textArea = document.querySelector("#text_area") ;
 
-button.addEventListener("click", () => {
-    window.alert("Hello there, welcome my website ...") ;
+
+//disable the send button
+button.style.disable = true;
+
+textArea.addEventListener("click", () => {
+    button.style.disable = false;
+    button.style.color = "#fff" ;
+    
+    
 });
+button.addEventListener("click", () => {
+    let user_input = textArea.value ;
+    
+    //create two paragraph elements
+    const user_text = document.createElement("p") ;
+    const bot_text = document.createElement("p") ;
+    
+    //set user paragraph 
+    user_text.setAttribute("class", "chat_paragraphs") ;
+    user_text.setAttribute("id", "user_message");
+    textSpace.appendChild(user_text) ;
+    user_text.textContent = user_input ;
+    
+    //set bot paragraph 
+    bot_text.setAttribute("class", "chat_paragraphs") ;
+    bot_text.setAttribute("id", "bot_message") ;
+    textSpace.appendChild(bot_text)
+    bot_text.textContent ="My Name is Vercel, an AI model developed by LANTEX ." ;
+    
+    
+});
+
+
